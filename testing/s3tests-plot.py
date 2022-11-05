dfce4557c693f0e3d93b426ce06216bbb469fe54
Joao Eduardo Luis <joao@suse.com>
2022-11-05 09:16:20 +0000

    testing/s3tests-plot: cleanup, use plots lib
    Signed-off-by: Joao Eduardo Luis <joao@suse.com>

--- testing/s3tests-plot.py
+++ testing/s3tests-plot.py
@@ -11,18 +11,16 @@
 # pyright: reportMissingTypeStubs=false
 
 import asyncio
-import math
 import re
 import sys
 from io import StringIO
 from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple
+from typing import Dict, List, Optional, Tuple
 
 import click
 import pandas
-import plotly.graph_objects as go
 import yaml
-from plotly.subplots import make_subplots
+from lib import plots
 
 
 async def _parse_log(logfile: Path) -> str:
@@ -55,112 +53,7 @@ async def plot_s3tests(
 
     csv_raw = await _parse_log(logpath)
     data = pandas.read_csv(StringIO(csv_raw), sep="\t")
-
-    # print(data.sort_values(by=["unit", "result"]))
-    df = (
-        data.groupby(by="result", group_keys=True)
-        .count()
-        .reset_index(names="result")
-    )
-
-    # fig = px.pie(
-    #     df,
-    #     values="unit",
-    #     names="result",
-    #     title="Results",
-    #     color="result",
-    #     color_discrete_map={
-    #         "ok": "green",
-    #         "fail": "orange",
-    #         "error": "darkred",
-    #     },
-    # )
-    # fig.write_image("s3tests-result.png")
-
-    filtered: Dict[str, pandas.DataFrame] = {}
-    for k, filter_lst in filters.items():
-        k_df: Optional[pandas.DataFrame] = None
-        for filter in filter_lst:
-            filter_df = data[data["unit"].str.match(filter)]
-            if filter_df.empty:
-                continue
-            if k_df is None:
-                k_df = filter_df
-            else:
-                k_df = pandas.concat([k_df, filter_df], verify_integrity=True)
-        if k_df is None:
-            print(f"unable to find matching units for section '{k}'")
-            continue
-        filtered[k] = k_df
-
-    # max 4 plots per row
-    nplots = len(filtered)
-    nrows = math.ceil(nplots / 4)
-    ncols = 4 if nplots >= 4 else nplots
-    ncols = 1 if ncols == 0 else ncols
-    totals_spec: List[Any] = [{"colspan": ncols, "type": "domain"}] + [
-        None for _ in range(ncols - 1)
-    ]
-    specs: List[List[Optional[Dict[str, Any]]]] = [totals_spec]
-    for _ in range(nrows):
-        specs.append([{"type": "domain"} for _ in range(ncols)])
-
-    result_colors = {
-        "ok": "yellowgreen",
-        "fail": "tomato",
-        "error": "slategray",
-    }
-
-    fig = make_subplots(
-        rows=nrows + 1,
-        cols=ncols,
-        specs=specs,
-        subplot_titles=["total"] + list(filtered.keys()),
-    )
-    # default height: 450px, ballpark height for one row: 225px
-    fig.update_layout(height=max(450, (nrows + 1) * 225), title_text=name)
-
-    total_labels: List[str] = []
-    total_values: List[str] = []
-    for l, v in df.values:
-        total_labels.append(l)
-        total_values.append(v)
-    fig.add_trace(
-        go.Pie(labels=total_labels, values=total_values, name="total"), 1, 1
-    )
-
-    col = 1
-    row = 2
-    for k, df in filtered.items():
-        if col > 4:
-            row += 1
-            col = 1
-
-        section_data = (
-            df.groupby(by="result").count().reset_index(names="result")
-        )
-        # print(section_data)
-        # print(f"labels: {section_data.index}, values: {section_data.values}")
-        labels: List[str] = []
-        values: List[int] = []
-        for l, v in section_data.values:
-            labels.append(l)
-            values.append(v)
-
-        marker_colors = section_data["result"].map(result_colors)
-        pie = go.Pie(
-            labels=labels,
-            values=values,
-            name=k,
-            textinfo="percent+value",
-            marker_colors=marker_colors,
-        )
-        fig.add_trace(pie, row, col)
-        col += 1
-
-    destfilename = name.lower().replace(" ", "-")
-    destfile = destpath.joinpath(f"{destfilename}-total.{format}")
-    fig.write_image(destfile)
+    plots.plot_s3test_results(data, filters, destpath, name, format)
 
 
 @click.command()
